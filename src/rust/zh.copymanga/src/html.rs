337e8b46fe6f52046af74284b3b4d38eea5bb1c0
lcl2a <131690795+lcl2a@users.noreply.github.com>
2026-05-25 02:11:05 +0200

    modified html

--- src/rust/zh.copymanga/src/html.rs
+++ src/rust/zh.copymanga/src/html.rs
@@ -78,8 +78,13 @@ pub trait MangaPage {
 impl MangaPage for Document {
 	fn update_details(&self, manga: &mut Manga) -> Result<()> {
 		manga.title = self
-			.try_select_first("h6")?
+			.try_select_first("li:contains(別名：) > p.comicParticulars-right-txt")?
 			.text()
+			.or_else(|| {
+				self.try_select_first("h6")
+					.ok()
+					.and_then(|element| element.text())
+			})
 			.ok_or_else(|| error!("Text not found"))?;
 
 		manga.cover = self
